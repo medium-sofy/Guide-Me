c06b68278fb5c0ed9214abe3c6d0c2be18aed196
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-16 07:56:05 +0200

    Add AuthInputs component

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { authModalState } from "@/atoms/authModalAtom";
+import { useRecoilValue } from "recoil";
+import { Flex } from "@chakra-ui/react";
+import Login from "./Login";
+
+type AuthInputsProps = {};
+
+const AuthInputs: React.FC<AuthInputsProps> = () => {
+  const modalState = useRecoilValue(authModalState);
+  return (
+    <Flex direction="column" align="center" width="100%" mt={4}>
+      {modalState.view === "login" && <Login/>}
+      {/* {modalState.view === "signup" && <SignUp/>} */}
+    </Flex>
+  );
+};
+export default AuthInputs;
