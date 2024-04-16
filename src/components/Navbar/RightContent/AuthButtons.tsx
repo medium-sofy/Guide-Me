1259ff0cb26ac0ebbe03becf7ce4f3dc40990b1b
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-16 07:48:43 +0200

    Add AuthButtons component

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,30 +1,32 @@
-import { Button } from '@chakra-ui/react';
-import React from 'react';
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
 
-type AuthButtonsProps = {
-    
+const AuthButtons: React.FC = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  return (
+    <>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "login" })}
+      >
+        Log In
+      </Button>
+      <Button
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "signup" })}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
 };
-
-const AuthButtons:React.FC = () => {
-    
-    return (
-        <>
-        <Button 
-            variant="outline"
-            height="28px"
-            display={{ base: "none", sm: "flex" }}
-            width={{ base: "70px", md: "110px" }}
-            mr={2}
-            onClick={() => {}}
-        >
-            Log In</Button>
-        <Button
-             height="28px"
-             display={{ base: "none", sm: "flex" }}
-             width={{ base: "70px", md: "110px" }}
-             mr={2}        
-        >Sign Up</Button>
-        </>
-    )
-}
 export default AuthButtons;
