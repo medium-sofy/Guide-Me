cd6c81de557ea5ed4e9fe137dc96900f3ff4ae9a
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-16 07:43:46 +0200

    Add recoil authModalAtom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from '../chakra/theme';
 import Layout from "@/components/Layout/Layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
         <Layout>
-            <Component {...pageProps} />
+          <Component {...pageProps} />
         </Layout>
-    </ChakraProvider>
+      </ChakraProvider>
+    </RecoilRoot>
   )
 }
