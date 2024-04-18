a276e8134a6c6518b523b514b6e0c98513f54f10
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-18 12:03:49 +0200

    Add UserMenu component to nav bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Flex, Image} from '@chakra-ui/react';
 import SearchInput from './SearchInput';
 import RightContent from './RightContent/RightContent';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const Navbar:React.FC = () => {
+  const [user, loading, error] = useAuthState(auth)
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
             <Flex alignItems="center">
@@ -12,7 +15,7 @@ const Navbar:React.FC = () => {
             </Flex>
              {/* {/* <Directory/> {'}'}  */}
             <SearchInput/>
-            <RightContent/>
+            <RightContent user={user}/>
                      
            
         </Flex>
