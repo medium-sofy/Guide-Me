96aac47e328afd8fb0394ce7c00c8a8bfc148cd8
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-18 19:19:53 +0200

    Set search input width depending on user login

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -6,31 +6,38 @@ import {
   InputRightElement,
 } from "@chakra-ui/input";
 import { Flex } from "@chakra-ui/layout";
+import { User } from "firebase/auth";
 import React from "react";
 
-type SearchInputProps = {};
+type SearchInputProps = {
+  user?: User | null ;
+};
 
-const SearchInput: React.FC<SearchInputProps> = () => (
-  <Flex flexGrow={1} mr={2} align="center">
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => (
+  <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
     <InputGroup>
-      <InputLeftElement pointerEvents="none"
-      children={<SearchIcon color="gray.400" mb={1} />}
-      />        
-      
-      <Input type="tel" placeholder="Search Guide Me" fontSize="10px" 
-      _placeholder={{color: "gray.500"}}
-      _hover={{
-        bg: "white",
-        border: "1px solid",
-        borderColor: "blue.500"
-      }}
-      _focus={{
-        outline:"none",
-        border:"1px solid",
-        borderColor: "blue.500"
-      }}
-      height="34px"
-      bg="gray.50"
+      <InputLeftElement
+        pointerEvents="none"
+        children={<SearchIcon color="gray.400" mb={1} />}
+      />
+
+      <Input
+        type="tel"
+        placeholder="Search Guide Me"
+        fontSize="10px"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        height="34px"
+        bg="gray.50"
       />
     </InputGroup>
   </Flex>
