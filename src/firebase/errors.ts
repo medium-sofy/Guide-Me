1cc408b44e979081b978fcf4098f7dca658b91c7
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-19 20:25:47 +0200

    Update errors.ts

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -2,5 +2,6 @@ export const FIREBASE_ERRORS = {
   'Firebase: Error (auth/email-already-in-use).': 'A user with that email already exists',
   "Firebase: Error (auth/user-not-found).": "Invalid email or password.",
   "Firebase: Error (auth/wrong-password).": "Invalid email or password.",
-  "Firebase: Error (auth/invalid-credential).": 'Invalid email or password.'
+  "Firebase: Error (auth/invalid-credential).": 'Invalid email or password.',
+  "Firebase: Error (auth/popup-closed-by-user).": 'Please try again, or sign up with email instead.'
 }
