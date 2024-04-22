4dc51b517e06e7be05a2898da3d686be3c07b8b7
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-22 18:16:58 +0200

    Add PageContent Layout component

---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,37 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type PageContentProps = { children?: JSX.Element[] };
+
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    <Flex justify="center" padding="16px 0px" border="1px solid red">
+      <Flex
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+        border="1px solid green"
+      >
+        {/* LHS */}
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border="1px solid blue"
+        >
+          {children && children[0]}
+        </Flex>
+        {/* RHS */}
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border="1px solid orange"
+        >
+          {children && children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default PageContent;
