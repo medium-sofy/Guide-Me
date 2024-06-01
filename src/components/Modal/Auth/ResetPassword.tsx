4b22a3e941686340dadcc617ff1aa9c54174d2f7
Mohamed Sofy <mediumsofy@gmail.com>
2024-06-02 01:08:20 +0300

    Fix build error

--- src/components/Modal/Auth/ResetPassword.tsx
+++ src/components/Modal/Auth/ResetPassword.tsx
@@ -7,10 +7,10 @@ import { auth } from "../../../firebase/clientApp";
 import { useSetRecoilState } from "recoil";
 
 type ResetPasswordProps = {
-  toggleView: (view: ModalView) => void;
+  // toggleView: (view: ModalView) => void;
 };
 
-const ResetPassword: React.FC<ResetPasswordProps> = ({ toggleView }) => {
+const ResetPassword: React.FC<ResetPasswordProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
   const [email, setEmail] = useState("");
   const [success, setSuccess] = useState(false);
